b63ad2515a9dea16b4262ee8461b73aec5b99411
Manami Batai <vaattii1996@gmail.com>
2025-12-18 21:01:31 +0900

    refactor [136] Single Number

--- 136.single-number.ts
+++ 136.single-number.ts
@@ -6,15 +6,24 @@
  */
 
 // @lc code=start
+// function singleNumber(nums: number[]): number {
+//   let result = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     // 同じ数字の個数を出す
+//     const count = nums.filter((num) => num === nums[i]).length;
+//     // 重複なしの場合
+//     if (count == 1) {
+//       return nums[i];
+//     }
+//   }
+//   return result;
+// }
+
+// XOR(排他的論理和)を使った解答
 function singleNumber(nums: number[]): number {
   let result = 0;
   for (let i = 0; i < nums.length; i++) {
-    // 同じ数字の個数を出す
-    const count = nums.filter((num) => num === nums[i]).length;
-    // 重複なしの場合
-    if (count == 1) {
-      return nums[i];
-    }
+    result ^= nums[i];
   }
   return result;
 }
