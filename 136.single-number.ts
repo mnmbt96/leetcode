6fb2e4252fa24d8e881e24c3d0407824d4ba9d26
Manami Batai <vaattii1996@gmail.com>
2025-12-18 20:57:29 +0900

    solved [136] Single Number

---
+++ 136.single-number.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=136 lang=typescript
+ *
+ * [136] Single Number
+ * https://leetcode.com/problems/single-number/description/
+ */
+
+// @lc code=start
+function singleNumber(nums: number[]): number {
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    // 同じ数字の個数を出す
+    const count = nums.filter((num) => num === nums[i]).length;
+    // 重複なしの場合
+    if (count == 1) {
+      return nums[i];
+    }
+  }
+  return result;
+}
+// @lc code=end
