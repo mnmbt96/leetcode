5f7ea029efe1c0dcd40cfab9ede446a4a2ec02d4
Manami Batai <vaattii1996@gmail.com>
2026-01-17 00:17:56 +0900

    add comment

--- 283.move-zeroes.ts
+++ 283.move-zeroes.ts
@@ -10,17 +10,20 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-  let insertPos = 0;
+  let insertPos = 0; // 数字を入れる場所
   let i = 0;
 
+  // 0以外を前に詰める
   while (i < nums.length) {
     if (nums[i] !== 0) {
+      // 0以外の数字の場合、insertPosに現在の要素を入れる
       nums[insertPos] = nums[i];
       insertPos++;
     }
     i++;
   }
 
+  // insertPos以降を全部0にする
   while (insertPos < nums.length) {
     nums[insertPos] = 0;
     insertPos++;
