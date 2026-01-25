00a5fcd8a12b00189133b9c88e69f1725c2e372d
Manami Batai <vaattii1996@gmail.com>
2026-01-25 23:40:08 +0900

    solved [344] Reverse String

---
+++ 344.reverse-string.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=344 lang=typescript
+ *
+ * [344] Reverse String
+ * https://leetcode.com/problems/increasing-triplet-subsequence/description/
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+  s.reverse();
+}
+// @lc code=end
