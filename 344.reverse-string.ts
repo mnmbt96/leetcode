d4ef5240be01330cd76b3ddddb4de799865cd49c
Manami Batai <vaattii1996@gmail.com>
2026-01-25 23:47:55 +0900

    refactor [344] Reverse String

--- 344.reverse-string.ts
+++ 344.reverse-string.ts
@@ -9,7 +9,19 @@
 /**
  Do not return anything, modify s in-place instead.
  */
+// function reverseString(s: string[]): void {
+//   s.reverse();
+// }
+
 function reverseString(s: string[]): void {
-  s.reverse();
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    [s[left], s[right]] = [s[right], s[left]];
+
+    left++;
+    right--;
+  }
 }
 // @lc code=end
